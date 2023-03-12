d1e069b4ae660dada8ee5d4a32692751fac089cf
HecFer5 <hecalfer@hotmail.com>
2023-03-12 11:34:24 -0230

    agrega ruta para achivo CSS correccion 4

--- myApp.js
+++ myApp.js
@@ -12,8 +12,8 @@ app.get("/", function (req, res) {
     res.sendFile(absolutePath)
 })
 //app.use(express.static(__dirname+ "/public"))
-app.use(express.static(__dirname + '/public'))
 
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
