4a034cf33a92e2d7453786cbe1c7e0f52771962b
HecFer5 <hecalfer@hotmail.com>
2023-03-18 16:35:42 -0230

    agregar variables

--- myApp.js
+++ myApp.js
@@ -1,55 +1,20 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
+  
+let mensaje =""
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      mensaje = "Hello World".toUpperCase();
+    } else {
+      mensaje = "Hello World";
+    }
 
-// app.get("/",function(req, res) {
-//     res.send("Hello Express");
-//   })
-// absolutePath = __dirname + '/views/index.html';
-
-// app.get("/", function (req, res) {
-
-//     res.sendFile(absolutePath)
-// })
-// //app.use(express.static(__dirname+ "/public"))
-
-// app.use("/public", express.static(__dirname + "/public"));
-// 
-app.get("/", function (req, res) {
-
-    res.json({"message": "Hello json"})
- //   your-app-url/json
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
+    app.get("/", function (req, res) {
+   
+        res.json( mensaje)
+    })
+ 
+console.log(mensaje)
+  
+    
+    app.listen(8080)
