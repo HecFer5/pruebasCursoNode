a002360cddb7f1b47128eba13f53b760796d6086
HecFer5 <hecalfer@hotmail.com>
2023-03-19 17:24:27 -0230

    recrea la ruta json

--- myApp.js
+++ myApp.js
@@ -1,20 +1,17 @@
-require('dotenv').config()
 let express = require('express');
 let app = express();
-  
-let mensaje =""
-    if (process.secrets.MESSAGE_STYLE === "uppercase") {
-      mensaje = "Hello World".toUpperCase();
-    } else {
-      mensaje = "Hello World";
-    }
 
-    app.get("/", function (req, res) {
-   
-        res.json( mensaje)
-    })
- 
-console.log(mensaje)
+// absolutePath = __dirname + '/views/index.html';
+// app.get("/", function (req, res) {
+//     res.sendFile(absolutePath)
+// })
+// app.use(express.static(__dirname + '/public'))
+
+//  app.use("/public", express.static(__dirname + "/public"));
+
+mensaje = {"message": "Hello json"}
+app.get('/', function(req, res) {
+    res.json({"message": "Hello json"});
+  })
   
-    
-   
+app.listen(8080)
